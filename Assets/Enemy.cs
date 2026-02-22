fc46c87d72a62b8bf90c37f303ad4be07932a1cc
nantathiwat <nantathiwat.t@ku.th>
2026-02-22 21:19:00 +0700

    03

--- Assets/Enemy.cs
+++ Assets/Enemy.cs
@@ -2,17 +2,58 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
-    private int livePoint = 3;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public int livePoint = 3;
+    public Transform player;
+    public float chaseSpeed = 2f;
+    public float jumpForce = 2f;
+    public LayerMask groundLayer;
+
+    private Rigidbody2D rb;
+    private bool isGrounded;
+    private bool shouldJump;
+
+    // Start is called before the first frame update
     void Start()
     {
-
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        // Is Grounded?
+        isGrounded = Physics2D.Raycast(transform.position, Vector2.down, 1f, groundLayer);
+
+        // Player Direction
+        float direction = Mathf.Sign(player.position.x - transform.position.x);
+
+        // Player above detection
+        bool isPlayerAbove = Physics2D.Raycast(transform.position, Vector2.up, 3f, 1 << player.gameObject.layer);
+
+        if (isGrounded)
+        {
+            // Chase player
+            rb.velocity = new Vector2(direction * chaseSpeed, rb.velocity.y);
+
+            // Jump if there's gap ahead && no ground infront
+            // else if there's player above and platform above
+
+            // If Ground
+            RaycastHit2D groundInFront = Physics2D.Raycast(transform.position, new Vector2(direction, 0), 2f, groundLayer);
+            // If gap
+            RaycastHit2D gapAhead = Physics2D.Raycast(transform.position + new Vector3(direction, 0, 0), Vector2.down, 2f, groundLayer);
+            // If platform above
+            RaycastHit2D platformAbove = Physics2D.Raycast(transform.position, Vector2.up, 3f, groundLayer);
+
+            if (!groundInFront.collider && !gapAhead.collider)
+            {
+                shouldJump = true;
+            }
+            else if (isPlayerAbove && platformAbove.collider)
+            {
+                shouldJump = true;
+            }
+        }
     }
 
     public void TakeDamage(int damage){
@@ -28,4 +69,16 @@ public class Enemy : MonoBehaviour
         }
     }
 
+    private void FixedUpdate()
+    {
+        if (isGrounded && shouldJump)
+        {
+            shouldJump = false;
+            Vector2 direction = (player.position - transform.position).normalized;
+
+            Vector2 jumpDirection = direction * jumpForce;
+
+            rb.AddForce(new Vector2(jumpDirection.x, jumpForce), ForceMode2D.Impulse);
+        }
+    }
 }
